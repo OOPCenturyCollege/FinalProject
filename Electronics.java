d1771be62993a19197edab0a8ece259c24507e95
kathawj <61756981+kathawj@users.noreply.github.com>
2020-04-29 14:48:17 -0500

    Update and rename Tops.java to Electronics.java

---
+++ Electronics.java
@@ -0,0 +1,27 @@
+
+package FinalPackage1;
+
+public class Electronics extends Items {
+	private double price;
+
+	
+	public Electronics(String name, double price) {
+		super(name, price);
+		this.price = price;
+
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	
+	@Override
+	public String toString() {
+		return name + " price=" + price + ", quantity=" +"]";
+	} 
+	
+	
+
+}
