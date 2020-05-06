0c3bd4c6b8223b18f786b927b28d4110b0f2f556
kevinpho970 <61472889+kevinpho970@users.noreply.github.com>
2020-05-06 18:36:03 -0500

    Update Customer.java

--- Customer.java
+++ Customer.java
@@ -23,78 +23,60 @@ public class Customer {
 		this.accountNumber = accountNumber;
 	}
 
+	//getters and setters
 	public String getName() {
 		return name;
 	}
-
 	public void setName(String name) {
 		this.name = name;
 	}
-
 	public String getLname() {
 		return lname;
 	}
-
 	public void setLname(String lname) {
 		this.lname = lname;
 	}
-
 	public String getAddress() {
 		return address;
 	}
-
 	public void setAddress(String address) {
 		this.address = address;
 	}
-
 	public String getCity() {
 		return city;
 	}
-
 	public void setCity(String city) {
 		this.city = city;
 	}
-
 	public String getState() {
 		return state;
 	}
-
 	public void setState(String state) {
 		this.state = state;
 	}
-
 	public String getEmail() {
 		return email;
 	}
-
 	public void setEmail(String email) {
 		this.email = email;
 	}
-
 	public int getZipcode() {
 		return zipcode;
 	}
-
 	public void setZipcode(int zipcode) {
 		this.zipcode = zipcode;
 	}
-
 	public int getAccountNumber() {
 		return accountNumber;
 	}
-
 	public void setAccountNumber(int accountNumber) {
 		this.accountNumber = accountNumber;
 	}
 
+
 	@Override
 	public String toString() {
-		return "\nCustomer" + "\n name: " + name + " last name: " + lname + " \n address: " + address + "\n city: " + city + "\n state: "
-				+ state + "\n email: " + email + " \n zipcode: " + zipcode + "\n accountNumber: " + accountNumber + "";
+		return " Your Account Info" + "\n First Name: " + name + "\n Last name: " + lname + " \n Address: " + address + "\n City: " + city + "\n State: "
+				+ state + "\n Email: " + email + " \n Zipcode: " + zipcode + "\n Account Number: " + accountNumber + "";
 	} 
-	
-	
-	
-	
-
 }
