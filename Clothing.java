0187c65d370aee1e83ff2ca1c772cddf3eb42917
kevinpho970 <61472889+kevinpho970@users.noreply.github.com>
2020-05-06 18:34:25 -0500

    Update Clothing.java

--- Clothing.java
+++ Clothing.java
@@ -2,29 +2,35 @@ package FinalPackage2;
 
 import FinalPackage1.Items;
 
-public class Consumables extends Items {
-	private double price; 
-	
-	public Consumables(String name, double price) {
-		super(name, price);
-		this.price = price;
+public class Clothing extends Items {
+	private double price;
+	private String name;
 
+	//constructor
+	public Clothing(String name, double price) {
+		super(name, price);
+		this.name = name;
+		this.price = price;
 	}
+	
+	//getters and setters
 	public double getPrice() {
 		return price;
 	}
 	public void setPrice(double price) {
 		this.price = price;
 	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
 	
 	@Override
 	public String toString() {
-		return  name + "[ price=" + price + "]";
+		return name + " | Price: $" + price;
 	} 
-	
-	
-	
-	
-	
-
 }
+
