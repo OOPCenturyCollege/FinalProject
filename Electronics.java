de0a15ba2fa3d6ce01207cd45a43bd0bece4dc9d
kevinpho970 <61472889+kevinpho970@users.noreply.github.com>
2020-05-06 18:36:38 -0500

    Update Electronics.java

--- Electronics.java
+++ Electronics.java
@@ -4,26 +4,34 @@ import FinalPackage1.Items;
 
 public class Electronics extends Items {
 	private double price;
+	private String name;
 
-	
+	//constructor
 	public Electronics(String name, double price) {
 		super(name, price);
+		this.name = name;
 		this.price = price;
-
 	}
+	
+	//getters and setters
 	public double getPrice() {
 		return price;
 	}
 	public void setPrice(double price) {
 		this.price = price;
 	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
 	
 	@Override
 	public String toString() {
-		return name + " [price=" + price +"]";
+		return name + " | Price: $" + price;
 	} 
-	
-	
-
 }
+
 
