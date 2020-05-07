89ffb4b4f673ed1cd6f9a4f65f5c5ec9e86593fc
kevinpho970 <61472889+kevinpho970@users.noreply.github.com>
2020-05-07 04:09:10 -0500

    Update Items.java

--- Items.java
+++ Items.java
@@ -1,50 +1,42 @@
-package FinalPackage1;
+package FinalPackage2;
 
 public class Items {
-	public String name; 
+	private String name; 
 	private double price;
 	private int quantity;
 	
-	public Items(String name, double price) {
+	public Items(String name, double price, int quantity) {
 		super();
 		this.name = name;
 		this.price = price;
-		
+		this.quantity = quantity;
 	}
 
 	public String getName() {
 		return name;
 	}
-
 	public void setName(String name) {
 		this.name = name;
 	}
-
 	public double getPrice() {
 		return price;
 	}
-
 	public void setPrice(double price) {
 		this.price = price;
 	}
-
 	public int getQuantity() {
 		return quantity;
 	}
-
 	public void setQuantity(int quantity) {
 		this.quantity = quantity;
 	}
-	
 	public double getTotal() {
 		return quantity * price; 
 	}
 
+	
 	@Override
 	public String toString() {
-		return "Items [name=" + name + ", price=" + price + ", quantity=" + quantity + "]";
+		return name + ", " + price + ", " + quantity;
 	}
-	
-	
-
 }
